c895439bbce2ebd114f21bc39bdccc35b04ef07b
anibalalpizar <anibal.alpizar14@gmail.com>
2024-11-01 21:06:20 -0600

    Implement user authentication check in HomeLayout component

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,10 +1,14 @@
-// import { getSession } from '@/lib/session';
+import { getAuthenticatedSession } from '@/lib/session';
+import { redirect } from 'next/navigation';
 
 export default async function HomeLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // const session = await getSession();
+  const user = await getAuthenticatedSession();
+
+  if (!user) return redirect('/login');
+
   return children;
 }
