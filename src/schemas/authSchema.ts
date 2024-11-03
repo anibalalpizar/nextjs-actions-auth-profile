65ba795fe2fa468af9c90a407d843e500db4bee7
anibalalpizar <anibal.alpizar14@gmail.com>
2024-11-02 18:01:52 -0600

    Add signup schema validation using Zod

---
+++ src/schemas/authSchema.ts
@@ -0,0 +1,8 @@
+import { PASSWORD_MIN_LENGTH } from '@/constants';
+import { z } from 'zod';
+
+export const signupSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string().min(PASSWORD_MIN_LENGTH),
+});
