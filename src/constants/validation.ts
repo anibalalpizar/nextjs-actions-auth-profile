962e9c79a80ec39b1781a7f105916a463668a185
anibalalpizar <anibal.alpizar14@gmail.com>
2024-11-04 17:20:55 -0600

    Add SALT_ROUNDS constant and update encrypt function to use it

--- src/constants/validation.ts
+++ src/constants/validation.ts
@@ -1,1 +1,2 @@
 export const PASSWORD_MIN_LENGTH = 8;
+export const SALT_ROUNDS = 10;
--- src/lib/encrypt.ts
+++ src/lib/encrypt.ts
@@ -1,7 +1,10 @@
+import { SALT_ROUNDS } from '@/constants/validation';
 import bcrypt from 'bcrypt';
 
-export const encrypt = async (password: string, saltRounds: number = 10) =>
-  await bcrypt.hash(password, saltRounds);
+export const encrypt = async (
+  password: string,
+  saltRounds: number = SALT_ROUNDS
+) => await bcrypt.hash(password, saltRounds);
 
 export const compare = async (password: string, hash: string) =>
   await bcrypt.compare(password, hash);
