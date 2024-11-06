0304bee389f8193105285001ecd860ca9774d366
anibalalpizar <anibal.alpizar14@gmail.com>
2024-11-05 23:05:19 -0600

    Reorder imports in HomeLayout for improved readability

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,6 +1,7 @@
-import { Navbar } from '@/components/layout/navbar';
-import { getAuthenticatedSession } from '@/lib/session';
 import { redirect } from 'next/navigation';
+import { getAuthenticatedSession } from '@/lib/session';
+
+import { Navbar } from '@/components/layout/navbar';
 
 export default async function HomeLayout({
   children,
