e6e9503589ae4ff6bc2572c5be89ede0ad494918
anibalalpizar <anibal.alpizar14@gmail.com>
2024-11-06 23:47:44 -0600

    Add utility function to get changed fields between two objects

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,3 +9,13 @@ export const ensureStartsWith = (stringToCheck: string, startsWith: string) =>
   stringToCheck.startsWith(startsWith)
     ? stringToCheck
     : `${startsWith}${stringToCheck}`;
+
+export const getChangedFields = <T extends object>(
+  original: T,
+  updated: T
+): Partial<T> => Object.keys(updated).reduce((changes, key) => {
+    if (updated[key as keyof T] !== original[key as keyof T]) {
+      changes[key as keyof T] = updated[key as keyof T];
+    }
+    return changes;
+  }, {} as Partial<T>);
