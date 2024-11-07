39c9e2aaf50575398bd42dd06a8cdb6b9db21609
anibalalpizar <anibal.alpizar14@gmail.com>
2024-11-06 23:56:04 -0600

    Refactor layout component to ensure children are rendered correctly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,7 +60,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <main>{children}</main>
+          {children}
           <Toaster />
         </ThemeProvider>
       </body>
