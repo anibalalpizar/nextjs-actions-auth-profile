1dd1b07fc7443ee0df137cb4b5d952dbe0eff23a
anibalalpizar <anibal.alpizar14@gmail.com>
2024-11-10 21:44:18 -0600

    Refactor getUser action to simplify user retrieval and remove unnecessary avatar processing

--- src/actions/profile/actions.ts
+++ src/actions/profile/actions.ts
@@ -9,15 +9,7 @@ import { prisma } from '@/lib/prisma';
 import { getChangedFields } from '@/lib/utils';
 import { profileSchema } from '@/schemas/profileSchema';
 
-export const getUser = async () => {
-  const user = await getAuthenticatedSession();
-
-  if (!user) return null;
-
-  if (user.avatar) user.avatar = user.avatar.toString('base64');
-
-  return user;
-};
+export const getUser = async () => getAuthenticatedSession();
 
 export const updateProfile = async (formData: FormData) => {
   const user = await getAuthenticatedSession();
@@ -27,33 +19,23 @@ export const updateProfile = async (formData: FormData) => {
 
   const parseData = profileSchema.safeParse(rawFormData);
 
-  if (!parseData.success) {
+  if (!parseData.success)
     return {
       error: 'Invalid data',
     };
-  }
 
-  const avatarFile = formData.get('avatar') as File | null;
-  let avatar: Buffer | null = null;
+  const changedFields = getChangedFields(user, {
+    ...user,
+    ...parseData.data,
+  });
 
-  if (avatarFile) avatar = Buffer.from(await avatarFile.arrayBuffer());
-
-  const changedFields = getChangedFields<typeof parseData.data>(
-    user,
-    parseData.data
-  );
-
-  if (avatar) changedFields.avatar = avatar;
-
-  if (Object.keys(changedFields).length === 0)
+  if (!changedFields)
     return {
       error: 'No changes detected',
     };
 
   await prisma.user.update({
-    where: {
-      id: user.id,
-    },
+    where: { id: user.id },
     data: changedFields,
   });
 
