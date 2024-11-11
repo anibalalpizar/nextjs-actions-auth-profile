70e9a586d9c4a384fe0d2c3870e05b7ab05dbf81
anibalalpizar <anibal.alpizar14@gmail.com>
2024-11-10 21:42:57 -0600

    Refactor profile page to fetch user data on mount and display loading skeletons

--- src/app/(home)/profile/page.tsx
+++ src/app/(home)/profile/page.tsx
@@ -1,8 +1,22 @@
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import ProfileForm from './profile-form';
+'use client';
+
+import { useEffect, useState } from 'react';
 import { getUser } from '@/actions/profile/actions';
 
-export default async function Page() {
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Skeleton } from '@/components/ui/skeleton';
+import ProfileForm from './profile-form';
+
+export default function Page() {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [user, setUser] = useState<any | null>(null);
+  useEffect(() => {
+    async function fetchUser() {
+      setUser(await getUser());
+    }
+    fetchUser();
+  }, []);
+
   return (
     <div className="flex items-center justify-center h-full p-4">
       <Card className="w-full max-w-md">
@@ -10,7 +24,15 @@ export default async function Page() {
           <CardTitle className="text-2xl font-bold">My Profile</CardTitle>
         </CardHeader>
         <CardContent className="space-y-6">
-          <ProfileForm user={await getUser()} />
+          {!user ? (
+            <div className="flex flex-col items-center space-y-4">
+              <Skeleton className="w-32 h-32 rounded-full" />
+              <Skeleton className="w-32 h-6" />
+              <Skeleton className="w-32 h-6" />
+            </div>
+          ) : (
+            <ProfileForm user={user} />
+          )}
         </CardContent>
       </Card>
     </div>
